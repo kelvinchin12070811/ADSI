0bb10ce02fa655a8bc43ad675006055d3492128e
Kelvin Chin <chinkaiwen@yahoo.com>
2021-02-12 13:40:23 +0800

    📜 Added license headers.

--- Encryptor/src/utils/DCT.cpp
+++ Encryptor/src/utils/DCT.cpp
@@ -1,6 +1,8 @@
-#include "DCT.hpp"
-#include "DCT.hpp"
-#include <algorithm>
+/**********************************************************************************************************************
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *********************************************************************************************************************/#include <algorithm>
 #include <boost/math/constants/constants.hpp>
 #include <iterator>
 #include <numeric>
