68a69a548d01dee28aac2c268d9ff9a55f5da992
Kelvin Chin <chinkaiwen@yahoo.com>
2021-03-06 07:58:18 +0800

    💄 Created basic UI

--- Encryptor/src/window/mainwindow/MainWindow.cpp
+++ Encryptor/src/window/mainwindow/MainWindow.cpp
@@ -3,6 +3,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *********************************************************************************************************************/
+#include <QGuiApplication>
+#include <QScreen>
+#include <QMessageBox>
+
 #include "window/mainwindow/MainWindow.hpp"
 
 namespace window
@@ -11,5 +15,14 @@ namespace window
         QMainWindow(parent), ui{ std::make_unique<Ui::MainWindow>() }
     {
         ui->setupUi(this);
+
+        auto largeWidth = QGuiApplication::primaryScreen()->size().width();
+        ui->splitSidebar->setStretchFactor(ui->splitSidebar->indexOf(ui->sidebarPanel), 0);
+        ui->splitSidebar->setStretchFactor(ui->splitSidebar->indexOf(ui->clientAreaPanel), 1);
+        ui->sidebarPanel->resize(200, ui->sidebarPanel->height());
+
+        connect(ui->btnImageLoad, &QPushButton::clicked, [this]() {
+            QMessageBox::information(this, "Hello", "Hello World!");
+        });
     }
 }
