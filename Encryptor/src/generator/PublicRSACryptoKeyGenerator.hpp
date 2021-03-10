27d9e015c75a1b7fd8585c58b5acfb2f4a62577f
Kelvin Chin <chinkaiwen@yahoo.com>
2021-03-10 08:01:09 +0800

    ✨ Setup boiler plate for public RSA key generator

---
+++ Encryptor/src/generator/PublicRSACryptoKeyGenerator.hpp
@@ -0,0 +1,23 @@
+/**********************************************************************************************************************
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *********************************************************************************************************************/
+#include "generator/RSACryptoKeyGeneratorBase.hpp"
+
+namespace key_generator
+{
+    /**
+     * @brief Public key generator of RSA algorithm.
+     */
+    class PublicRSACryptoKeyGenerator : public RSACryptoKeyGeneratorBase
+    {
+    public:
+    private:
+        /**
+         * @name Attributes
+         * @{
+         */
+        /** @} */
+    };
+}
