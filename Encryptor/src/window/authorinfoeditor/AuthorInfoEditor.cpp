f3f8f9174e2843438c82cb2ff5ea60d7ac8ca1f6
Kelvin Chin <chinkaiwen@yahoo.com>
2021-04-01 23:37:17 +0800

    💄 Added AuthorInfoEditor

--- Encryptor/src/window/authorinfoeditor/AuthorInfoEditor.cpp
+++ Encryptor/src/window/authorinfoeditor/AuthorInfoEditor.cpp
@@ -3,6 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *********************************************************************************************************************/
+#include "utils/StylesManager.hpp"
 #include "window/authorinfoeditor/AuthorInfoEditor.hpp"
 
 namespace window {
@@ -11,6 +12,6 @@ AuthorInfoEditor::AuthorInfoEditor(QWidget *parent)
 {
     ui_->setupUi(this);
 
-    
+    utils::StylesManager::getInstance().applyStylesheets(this);
 }
 }
